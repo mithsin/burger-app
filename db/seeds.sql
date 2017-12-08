e51086eb11157b13c34d02342db6e91734ffaae5
mithsin <hsinbluemoon@gmail.com>
2017-12-08 18:52:03 -0500

    good burger

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Apple');
+INSERT INTO burgers (name) VALUES ('pineapple');
+INSERT INTO burgers (name) VALUES ('meat Ball');
+INSERT INTO burgers (name, devoured) VALUES ('AppleB', true);
+INSERT INTO burgers (name, devoured) VALUES ('PineappleB', true);
+INSERT INTO burgers (name, devoured) VALUES ('MBB', true);
